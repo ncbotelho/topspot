bda2b9bc0ec979bddabdd74f9fd99c440356e303
ncbotelho <nathanbotelho@gmail.com>
2026-05-13 23:11:45 -0300

    Enhance landing page with improved layout and button components. Added a separator for better visual structure, updated job category buttons, and modified the job posting section for clarity. Adjusted button styles for consistency.

--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -1,26 +1,42 @@
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row w-full h-screen overflow-y-auto hidescroll">
       <div className="flex flex-col w-full md:w-xs md:sticky md:top-0 md:h-screen md:shrink-0 p-4 justify-between md:overflow-y-auto hidescroll">
-        <div className="flex flex-col gap-2">
-          <div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
             <h1 className="select-none">
               welove<span className="text-red-700">it</span>.
             </h1>
+            <p className="text-sm text-muted-foreground">A curated list of ERP & HRIS jobs</p>
           </div>
-          <div>
-            <p className="text-sm text-muted-foreground">A curated list of the best IT jobs</p>
+          <Separator />
+          <div className="flex flex-col">
+            <Button variant="outline" className="rounded-none grow border-b-0">
+              All Jobs
+            </Button>
+            <div className="flex flex-wrap">
+              <Button variant="outline" className="rounded-none grow">
+                SAP
+              </Button>
+              <Button variant="outline" className="rounded-none grow border-l-0">
+                Oracle
+              </Button>
+              <Button variant="outline" className="rounded-none grow border-l-0">
+                Workday
+              </Button>
+            </div>
           </div>
-          <div className="flex gap-2">
-            <Button>SAP</Button>
-            <Button>Oracle</Button>
-            <Button>Workday</Button>
+          <div className="flex flex-col">
+            <Button variant="outline" className="rounded-none grow">
+              Companies
+            </Button>
           </div>
         </div>
         <div>
-          <p>We love IT jobs. We love IT people. We love IT companies.</p>
+          <Button>Post a job</Button>
         </div>
       </div>
       <div className="flex flex-col flex-1 p-4">
