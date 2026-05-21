28d87d3f6d5a305d9c649ba4b1ebbb4eaa19bc62
ncbotelho <nathanbotelho@gmail.com>
2026-05-21 01:14:46 -0300

    Refactor landing page by consolidating header and main content structure. Removed redundant Home2 component and integrated search functionality into the main header. Updated global CSS for border styling consistency. Enhanced user experience with improved button placements and layout adjustments.

--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -1,102 +1,34 @@
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
 
-export function JobCard({ jobId, title, tags, salaryRange, company, location, url }: { jobId: string; title: string; tags: string[]; salaryRange?: string; company: string; location: string; url: string }) {
-  return (
-    <article id={jobId} className="flex w-full h-fit p-4 gap-8 justify-between items-end border-b last:border-b-0">
-      <div className="flex flex-col w-full gap-0.5">
-        <div className="flex flex-wrap items-center gap-4">
-          <h3 className="line-clamp-1">{title}</h3>
-          <div className="flex flex-wrap items-center gap-1">
-            {tags.map((tag) => (
-              <Badge key={`${jobId}-${tag}`} variant="secondary">
-                {tag}
-              </Badge>
-            ))}
-          </div>
-        </div>
-        {salaryRange && <p className="text-sm text-muted-foreground">{salaryRange}</p>}
-        <a href={`/companies/${company}`} className="w-fit">
-          <p className="text-sm text-muted-foreground hover:text-foreground line-clamp-2">{company}</p>
-        </a>
-        <a href={`/locations/${location}`} className="w-fit">
-          <p className="text-sm text-muted-foreground hover:text-foreground line-clamp-2">{location}</p>
-        </a>
-      </div>
-      <div className="flex items-center gap-2">
-        <Button
-          nativeButton={false}
-          variant="outline"
-          className="grow border-foreground"
-          render={
-            <a href={url}>
-              More <Plus />
-            </a>
-          }
-        />
-        <Button nativeButton={false} render={<a href={url}>Apply</a>} />
-      </div>
-    </article>
-  );
-}
+import { SearchIcon } from "lucide-react";
 
-export default function Home() {
+export default function Home2() {
   return (
-    <main className="flex flex-col md:flex-row w-full h-screen overflow-y-auto hidescroll">
-      <div className="flex flex-col w-full md:w-xs md:sticky md:top-0 md:h-screen md:shrink-0 p-4 justify-between md:overflow-y-auto hidescroll border-r border-foreground">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <div className="flex items-end h-12">
-              <h1 className="select-none">
-                TOPSPOT<span className="text-base text-primary tracking-tight">HCM</span>
-              </h1>
-            </div>
-            <p className="text-sm text-muted-foreground">A curated list of HCM jobs.</p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <Button variant="outline" className="grow border-foreground">
-              Home
-            </Button>
-            <Button variant="outline" className="grow border-foreground">
-              Jobs
-            </Button>
-            <Button variant="outline" className="grow border-foreground">
-              Companies
-            </Button>
-            <Button variant="outline" className="grow border-foreground">
-              Locations
-            </Button>
-            <Button variant="outline" className="grow border-foreground">
-              Skills
-            </Button>
-          </div>
+    <>
+      <header className="grid w-full grid-cols-3 sticky top-0 z-10 items-center py-1 px-4">
+        <div className="flex justify-start">
+          <h1 className="select-none">
+            TOPSPOT<span className="text-base text-primary tracking-tight">HCM</span>
+          </h1>
         </div>
-        <div className="flex gap-2">
-          <Button className="grow">Post a job</Button>
-          <form action="/login" method="GET">
-            <Button type="submit" variant="outline" className="grow border-foreground">
-              Login
-            </Button>
-          </form>
+        <div className="font-heading">
+          <InputGroup className="py-5 gap-2">
+            <InputGroupInput placeholder="Search jobs and companies" className="text-base!" />
+            <InputGroupAddon>
+              <SearchIcon />
+            </InputGroupAddon>
+          </InputGroup>
         </div>
-      </div>
-      <div className="flex flex-col flex-1 py-4">
-        <section className="flex flex-col h-full gap-4">
-          <div className="flex items-end h-12 gap-2 text-ring px-4">
-            <h2>All</h2>
-            <h2 className="text-foreground">Featured</h2>
-            <h2>Latest</h2>
-          </div>
-          <div>
-            <JobCard jobId="1" title="SuccessFactors Consultant" company="ComTec Information Systems (IT)" location="Newark, NJ" tags={["Employee Central", "10+ years of experience", "Full-time", "Remote"]} url="#" />
-            <JobCard jobId="2" title="SuccessFactors Consultant" company="MARVEL Infotech Inc." location="Newark, NJ" tags={["Employee Central, CPI", "5+ years of experience", "Full-time", "Hybrid"]} url="#" />
-            <JobCard jobId="3" title="SuccessFactors Specialist" company="Clevanoo LLC" location="Somerset County, NJ" tags={["Employee Central, Compensation", "5+ years of experience", "Full-time", "On-site"]} url="#" salaryRange="$150,000/yr" />
-            <JobCard jobId="4" title="Senior IT SuccessFactors Specialist" company="Clevanoo LLC" location="Piscataway, NJ" tags={["Employee Central", "5+ years of experience", "Full-time", "Hybrid"]} url="#" />
-            <JobCard jobId="5" title="SuccessFactors Senior/Lead Consultant" company="AspireHR" location="Dallas, TX" tags={["Onboarding", "3+ years of experience", "Full-time", "Remote"]} url="#" />
-          </div>
-        </section>
-      </div>
-    </main>
+        <div className="flex justify-end gap-2">
+          <Button variant="secondary">Login</Button>
+          <Button>Post a job</Button>
+        </div>
+      </header>
+      <main className="">
+        <section className=""></section>
+        <section className=""></section>
+      </main>
+    </>
   );
 }
