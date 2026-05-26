19e766fb1ac75b606d36fbfb6545d5c9394fa3d7
ncbotelho <nathanbotelho@gmail.com>
2026-05-26 00:09:16 -0300

    Update dependencies in package.json and bun.lock for @base-ui/react, @convex-dev/workos, lucide-react, and shadcn. Enhance landing page layout with additional sections for ecosystem, experience level, location, and contract type. Adjust global CSS for typography improvements and header styling.

--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -6,13 +6,13 @@ import { SearchIcon } from "lucide-react";
 export default function Home2() {
   return (
     <>
-      <header className="grid w-full grid-cols-3 sticky top-0 z-10 items-center py-1 px-4">
-        <div className="flex justify-start">
+      <header className="grid w-full lg:grid-cols-3 grid-cols-2 sticky top-0 z-10 items-center py-4 px-8">
+        <div className="flex w-fit justify-start">
           <h1 className="select-none">
             TOPSPOT<span className="text-base text-primary tracking-tight">HCM</span>
           </h1>
         </div>
-        <div className="font-heading">
+        <div className="flex-1 font-heading hidden lg:flex">
           <InputGroup className="py-5 gap-2">
             <InputGroupInput placeholder="Search jobs and companies" className="text-base!" />
             <InputGroupAddon>
@@ -25,9 +25,68 @@ export default function Home2() {
           <Button>Post a job</Button>
         </div>
       </header>
-      <main className="">
-        <section className=""></section>
-        <section className=""></section>
+      <main>
+        <section className="flex w-full py-12">
+          <div className="flex-1 px-8">
+            <p className="text-muted-foreground">ecosystem</p>
+            <ul>
+              <li>SAP</li>
+              <li>Workday</li>
+              <li>Dayforce</li>
+              <li>UKG</li>
+              <li>Cornerstone</li>
+            </ul>
+          </div>
+          <div className="flex-1 px-8">
+            <p className="text-muted-foreground">experience level</p>
+            <ul>
+              <li>3 years</li>
+              <li>5 years</li>
+              <li>10 years</li>
+              <li>15 years</li>
+              <li>20 years</li>
+            </ul>
+          </div>
+          <div className="flex-1 px-8">
+            <p className="text-muted-foreground">location</p>
+            <ul>
+              <li>Texas</li>
+              <li>New York</li>
+              <li>California</li>
+              <li>Washington</li>
+              <li>Illinois</li>
+            </ul>
+          </div>
+          <div className="flex-1 px-8">
+            <p className="text-muted-foreground">contract type</p>
+            <ul>
+              <li>Full-time</li>
+              <li>Part-time</li>
+              <li>Temporary</li>
+              <li>Employee</li>
+              <li>Contract</li>
+            </ul>
+          </div>
+          <div className="flex-1 px-8">
+            <p className="text-muted-foreground">contract type</p>
+            <ul>
+              <li>Full-time</li>
+              <li>Part-time</li>
+              <li>Temporary</li>
+              <li>Employee</li>
+              <li>Contract</li>
+            </ul>
+          </div>
+        </section>
+        <section className="px-8">
+          <div className="flex w-full justify-between items-center bg-muted rounded-md p-2">
+            <p>123</p>
+            <p>123</p>
+          </div>
+        </section>
+        <section className="py-12">
+          <div className="px-8">123</div>
+        </section>
       </main>
     </>
   );
